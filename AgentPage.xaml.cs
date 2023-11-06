f55d8d37d63833211e713a306a7c9d063b613980
Berdin Andrei <byte.in4matic@gmail.com>
2023-11-06 15:31:48 +0500

    12 лаба

--- AgentPage.xaml.cs
+++ AgentPage.xaml.cs
@@ -20,7 +20,12 @@ namespace Berdin_Glazki
     /// </summary>
     public partial class AgentPage : Page
     {
+        int CountRecords;
+        int CountPage;
+        int CurrentPage;
 
+        List<Agent> CurrentPageList = new List<Agent>();
+        List<Agent> TableList;
         public AgentPage()
         {
             InitializeComponent();
@@ -88,7 +93,98 @@ namespace Berdin_Glazki
                 currentAgent = currentAgent.Where(p => p.AgentTypeID == 6).ToList();
             }
             AgentListView.ItemsSource = currentAgent;
+            TableList = currentAgent;
+            ChangePage(0, 0);
 
+        }
+
+        private void ChangePage(int direction, int? selectedPage)
+        {
+            CurrentPageList.Clear();
+            CountRecords = TableList.Count;
+
+            if (CountRecords % 10 > 0)
+            {
+                CountPage = CountRecords / 10 + 1;
+            }
+            else
+            {
+                CountPage = CountRecords / 10;
+            }
+
+
+            Boolean Ifupdate = true;
+
+            int min;
+
+            if (selectedPage.HasValue)
+            {
+                if (selectedPage >= 0 && selectedPage <= CountPage)
+                {
+                    CurrentPage = (int)selectedPage;
+                    min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                    for (int i = CurrentPage * 10; i < min; i++)
+                    {
+                        CurrentPageList.Add(TableList[i]);
+                    }
+                }
+            }
+            else
+            {
+                switch (direction)
+                {
+                    case 1:
+                        if (CurrentPage > 0)
+                        {
+                            CurrentPage--;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            Ifupdate = false;
+                        }
+                        break;
+
+                    case 2:
+                        if (CurrentPage < CountPage - 1)
+                        {
+                            CurrentPage++;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            Ifupdate = false;
+                        }
+                        break;
+
+                }
+            }
+
+            if (Ifupdate)
+            {
+                PageListBox.Items.Clear();
+
+                for (int i = 1; i <= CountPage; i++)
+                {
+                    PageListBox.Items.Add(i);
+                }
+                PageListBox.SelectedIndex = CurrentPage;
+
+                min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                TBCount.Text = min.ToString();
+                TBAllRecords.Text = " из " + CountRecords.ToString();
+                AgentListView.ItemsSource = CurrentPageList;
+
+                AgentListView.Items.Refresh();
+            }
 
         }
 
@@ -110,6 +206,21 @@ namespace Berdin_Glazki
         private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
             UpdateAgents();
+        }
+
+        private void LeftDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(1, null);
+        }
+
+        private void PageListBox_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ChangePage(0, Convert.ToInt32(PageListBox.SelectedIndex.ToString()));
+        }
+
+        private void RightDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(2, null);
         }
     }
 }
